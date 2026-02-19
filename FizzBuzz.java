f0d9584aba01b455938468482393da17c98f06b3
ivancamilo1303-rgb <ivancamilo1303@gmail.com>
2026-02-19 10:56:39 -0500

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FizzBuzz {
+public class Principal {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -9,7 +9,7 @@ public class FizzBuzz {
         System.out.println("Ingrese numeros (0 para salir):");
 
         while (true) {
-            System.out.print("Numero: ");
+            System.out.print("Número: ");
             numero = scanner.nextInt();
 
             if (numero == 0) {
@@ -18,11 +18,11 @@ public class FizzBuzz {
             }
 
             if (numero % 3 == 0 && numero % 5 == 0) {
-                System.out.println("fizzbuzz");
+                System.out.println("FizzBuzz");
             } else if (numero % 3 == 0) {
-                System.out.println("fizz");
+                System.out.println("Fizz");
             } else if (numero % 5 == 0) {
-                System.out.println("buzz");
+                System.out.println("Buzz");
             } else {
                 System.out.println(numero);
             }
